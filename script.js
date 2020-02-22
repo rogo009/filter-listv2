6c29f47d46ae10eb527bb4775555f2fc47e1044f
robin <rogo009git@gmail.com>
2020-02-21 17:37:18 -0800

    update script

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const form = document.getElementById('names');
 filterInput.addEventListener('keyup', filterNames);
 
 function filterNames() {
+
+    // get value of input
     let filterValue = document.getElementById('filterInput').value.toUpperCase();
     console.log(filterValue);
+
+    // get names ul
+    let names = document.getElementById('names');
+    // get lis from ul
+    let li = ul.querySelectAll();
 }
 
 
-
+//I tried grabbing filterInput and it's value and converting it to upper case, but for some reason that doesn't work. You need to use a brand new variable assigned to the new value that it becomes. in this case it becomes all uppercase, you can't over write filterInput, so you have to assign it to a new variable name
