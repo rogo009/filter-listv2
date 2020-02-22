5b37a46e15b9500d1ca844a920b0c5f22a395dae
robin <rogo009git@gmail.com>
2020-02-21 18:00:09 -0800

    update script file

--- script.js
+++ script.js
@@ -10,9 +10,21 @@ function filterNames() {
     console.log(filterValue);
 
     // get names ul
-    let names = document.getElementById('names');
+    let ul = document.getElementById('names');
     // get lis from ul
-    let li = ul.querySelectAll();
+    let li = ul.querySelectorAll('li.collection-item');
+
+    // loop through collection-item lis
+    for(let i = 0; i < li.length; i++ ){
+        let a = li[i].getElementsByTagName('a')[0];
+        // if matched
+        if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+
+        }
+    }
 }
 
 
